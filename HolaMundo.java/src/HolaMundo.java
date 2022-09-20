22132b61e315a0b52a1e03e1d0a8cffb5b472d68
Andres Forero <izan884@gmail.com>
2022-09-19 22:17:09 -0500

    java numeros enteros y primitivos

--- HolaMundo.java/src/HolaMundo.java
+++ HolaMundo.java/src/HolaMundo.java
@@ -4,6 +4,42 @@ import java.util.Scanner;
 //mi clase java
 public class HolaMundo {
     public static void main (String args[]){
+        //Tipos  primitivos de tipo flotante, y Double
+        float numeroFloat = 10.0f;
+        System.out.println("Valor float = " + numeroFloat);
+        System.out.println("Valor minimo byte = " + Float.MIN_VALUE);
+        System.out.println("Valor maximo byte = " + Float.MAX_VALUE);        
+        
+        //Tipos primitivos bytes, short, int, long
+        /*
+        byte numeroByte = (byte)129;
+        System.out.println("Valor byte = " + numeroByte);
+        System.out.println("Valor minimo byte = " + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte = " + Byte.MAX_VALUE);
+        
+        short numeroShort = 10;
+        System.out.println("numero Short = " + numeroShort);
+        System.out.println("Valor minimo short = " + Short.MIN_VALUE);
+        System.out.println("Valor maximo short = " + Short.MAX_VALUE);       
+                
+                
+        int numeroInt = 10;
+        System.out.println("numero int = " + numeroInt);
+        System.out.println("Valor minimo numeroInt = " + Integer.MIN_VALUE);
+        System.out.println("Valor maximo numeroInt = " + Integer.MAX_VALUE);        
+                
+                
+        long numeroLong = 10;
+        System.out.println("numero Log = " + numeroLong);
+        System.out.println("Valor minimo long = " + Long.MIN_VALUE);
+        System.out.println("Valor maximo long = " + Long.MAX_VALUE);        
+          */      
+        
+        
+        
+        
+        /*========================================================*/
+        /*
         System.out.println("Escribe tu nombre : ");
         Scanner consola = new Scanner(System.in);
         var usuario = consola.nextLine();
@@ -11,7 +47,7 @@ public class HolaMundo {
         System.out.println("Escribe el titulo : ");
         var titulo = consola.nextLine();
         System.out.println("Resultado = " + titulo +" " + usuario);
-        
+        */
         
         
         /*========================================================*/
